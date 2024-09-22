48874e2bf83485c81508c1a80b32a3754116ce4b
michel-mexique <michel-mexique@proton.me>
2024-09-22 22:45:17 +0000

    first

---
+++ make_src_dist.sh
@@ -0,0 +1,32 @@
+#/bin/bash
+#
+# This script is used to create a clean source distribution (starting from 2.0.0)
+#
+
+REL=2.2.0
+
+DIR="pfstools-${REL}"
+DEST="${DIR}.tgz"
+
+echo "Preparing ${DEST}"
+
+cd ..
+
+# Create a clean copy
+rm -rf ${DIR}
+rm -rf ${DEST}
+cp -r pfstools ${DIR}
+
+# Remove unnecessary files
+rm -rf ${DIR}/.git
+rm -rf ${DIR}/.nfs*
+rm -r ${DIR}/debian
+rm -r ${DIR}/build
+rm ${DIR}/TODO
+rm ${DIR}/make_src_dist.sh
+
+# Put into .tgz
+tar -czf ${DEST} ${DIR}
+
+# Test
+tar -tzf ${DEST} ${DIR}
